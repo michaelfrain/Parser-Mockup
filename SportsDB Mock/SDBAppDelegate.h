f44515760af4676c869f84c4f4764650afd46094
Michael Frain <michael.frain@mobileforming.com>
2014-01-12 09:13:48 -0800

    Initial Commit

---
+++ SportsDB Mock/SDBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SDBAppDelegate.h
+//  SportsDB Mock
+//
+//  Created by Michael Frain on 1/12/14.
+//  Copyright (c) 2014 SportsDB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SportsDB Mock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SportsDB Mock
+//
+//  Created by Michael Frain on 1/12/14.
+//  Copyright (c) 2014 SportsDB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDBAppDelegate class]));
+    }
+}
