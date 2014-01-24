5151c076fa685d2a37a0d7947b04cc2d898f13f5
Michael Frain <michael.frain@mobileforming.com>
2014-01-23 22:57:42 -0800

    adding ui to demonstrate parser functionality

---
+++ SportsDB Mock/SDBViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDBViewController.h
+//  SportsDB Mock
+//
+//  Created by Michael Frain on 1/23/14.
+//  Copyright (c) 2014 SportsDB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDBViewController : UIViewController
+
+@end
---
+++ SportsDB Mock/SDBViewController.m
@@ -0,0 +1,38 @@
+//
+//  SDBViewController.m
+//  SportsDB Mock
+//
+//  Created by Michael Frain on 1/23/14.
+//  Copyright (c) 2014 SportsDB. All rights reserved.
+//
+
+#import "SDBViewController.h"
+
+@interface SDBViewController ()
+
+@end
+
+@implementation SDBViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
